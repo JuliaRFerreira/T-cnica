2c464381c4de022bffc338c3d76dcc7cad09924e
Júlia Rodrigues Ferreira <144164872+JuliaRFerreira@users.noreply.github.com>
2024-09-03 15:03:06 -0300

    Update teste5.java

--- teste5.java
+++ teste5.java
@@ -1,5 +1,17 @@
 package estagio;
 
-public class teste5 {
+//Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. Você não pode ver as lâmpadas da sala em que está, 
+//mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada. 
+//Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada? 
 
-}
+
+//Ligue o primeiro interruptor e vai pra sala das lâmpas.
+// A lâmpada que tiver ligada é a do primeiro interruptor.
+//ligue o seundo interruptor e vai pra sala das lâmpadas.
+//a segunda lâmpada ligada, vai ser a do segundo interruptor.
+//por tanto o interruptor que vc não ligou, é a da luz apagada.
+
+
+//A lâmpada que está acesa é controlada pelo primeiro interruptor.
+//A lâmpada que acendeu depois da primeira lâmpada acessa,  é controlada pelo segundo interruptor.
+//A lâmpada que está apagada é controlada pelo terceiro interruptor.
